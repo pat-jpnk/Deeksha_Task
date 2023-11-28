8c9e0822373f87377a3336019056e8cd920417c2
pat-jpnk <jpnksoftware@protonmail.com>
2023-11-28 04:59:38 +0100

    additions

--- FileEditor.py
+++ FileEditor.py
@@ -5,6 +5,8 @@ import pandas
 import openpyxl
 import filetype
 import uuid
+import xml.dom.minidom as md
+import xml.parsers
 
 '''
 Write a program to update/create a text file.
@@ -123,7 +125,7 @@ class TextFileEditor(FileEditor):
 class ExcelFileEditor(FileEditor):
 
     file_signature = ""
-    file_extensions = ["xlsx", "xlsm"]     # use list, more types ?
+    file_extensions = ["xlsx", "xlsm"]     # use list, more types ? TODO
 
     def create(self, path, name):
         if not os.path.isdir(path):
@@ -132,7 +134,7 @@ class ExcelFileEditor(FileEditor):
         file_name_split = name.split('.')
 
         if len(file_name_split) != 2 or file_name_split[1] not in ExcelFileEditor.file_extensions:      # limit to xlsx ? check reqs
-            raise ValueError("invalid filename, .txt extension required")
+            raise ValueError("invalid filename, .xlsx extension required")
 
         file_path = os.path.join(path, name)
 
@@ -144,11 +146,7 @@ class ExcelFileEditor(FileEditor):
 
 
     # two sheets in excel can't have the same name
-    def update(self, filepath, data):
-
-        # TODO: check data is DF
-        # TODO: openpyxl errors catch
-        # TODO: unique sheet name
+    def update(self, filepath, data, sheet_id):
 
         if not isinstance(data, pandas.DataFrame):
             raise TypeError("data is required to be a pandas DataFrame")
@@ -157,8 +155,6 @@ class ExcelFileEditor(FileEditor):
             raise FileNotFoundError("file does not exists")
 
         file_type = filetype.guess_extension(filepath)
-
-        print(file_type)
 
         if file_type:
             if file_type not in ExcelFileEditor.file_extensions:
@@ -181,18 +177,105 @@ class ExcelFileEditor(FileEditor):
     def verify_file_signature(self, filepath):
         pass
 
-# .xml
+
+# creating XML document without Document Type Definition
+# https://bspaans.github.io/python-mingus/_modules/xml/dom/minidom.html
+# https://github.com/python/cpython/blob/main/Lib/xml/dom/expatbuilder.pys
 class XMLFileEditor(FileEditor):
 
     file_signature = ""
-    file_extensions = ".txt"
+    file_extensions = ["xml"]
 
-    def create(self, path, name):
-        pass 
+    def create(self, path, name, xml_root):
+        if not os.path.isdir(path):
+            raise FileNotFoundError("directory does not exist")
+        
+        file_name_split = name.split('.')
 
-    def update(self, filepath, XXXX):
-        pass
-    
+        if len(file_name_split) != 2 or file_name_split[1] not in XMLFileEditor.file_extensions:   
+            raise ValueError("invalid filename, .xml extension required")
+
+        file_path = os.path.join(path, name)
+
+        if os.path.isfile(file_path):
+            raise FileExistsError("could not create file, already exists")
+
+        #xml_document = md.getDOMImplementation().createDocument(None, 'root', None)
+
+        xml_document = md.Document()
+
+        #root = xml_document.createElement("User")
+        #root.setAttribute( "id", 'myIdvalue' )
+        #root.setAttribute( "email", 'blabla@bblabla.com' )
+ 
+        #xml_document.appendChild(root)
+
+        
+        root_element = xml_document.createElement(xml_root)
+
+        xml_document.appendChild(root_element)
+        #text = xml_document.createTextNode("")
+        #root_element.appendChild(text)
+
+
+        #productChild = xml_document.createElement('connection')
+        #productChild.setAttribute('formatted-name', 'Federated') 
+        #productChild.setAttribute('inline', 'true') 
+        #root_element.appendChild(productChild)
+
+        '''
+        root_element = xml_document.createElement("tree")
+
+        xml_document.appendChild(root_element)
+
+        
+        productChild = xml_document.createElement('connection')
+        productChild.setAttribute('formatted-name', 'Federated') 
+        productChild.setAttribute('inline', 'true') 
+        root_element.appendChild(productChild)
+
+        text = xml_document.createTextNode("boo")
+        productChild.appendChild(text)
+        '''
+
+        xml_content = xml_document.toprettyxml(indent='\t')
+
+        with open(file_path, 'w') as f:
+            f.write(xml_content)
+
+
+    def update(self, filepath, element_name, element_text):
+
+        # updating restricted to adding elements that contain to the root element
+        # due to time reasons
+
+        if not os.path.isfile(filepath):
+            raise FileNotFoundError("file does not exists")
+        
+        if is_binary(filepath):
+            raise ValueError("provided file is binary, text file required")
+
+        with open(filepath, 'r') as f:
+            file_content = f.read()
+        
+        try:
+            content_dom = md.parseString(file_content)
+        except xml.parsers.expat.ExpatError as error:
+            print(error)
+
+
+        root_element = content_dom.documentElement
+        new_element = content_dom.createElement(element_name)
+        new_element_text = content_dom.createTextNode(element_text)
+        root_element.appendChild(new_element)
+        new_element.appendChild(new_element_text)
+
+        xml_content = content_dom.toprettyxml(indent='\t')
+
+        with open(filepath, 'w') as f:
+            f.write(xml_content)
+
+
     def verify_file_extension(self, filepath):
         pass
 
@@ -206,4 +289,16 @@ if __name__ == "__main__":
     d.create("/home/patrick/Desktop/KPIT", "file.xlsx")
 
     d.update("/home/patrick/Desktop/KPIT/file.xlsx", "Hi")
+    '''
+
+    d = XMLFileEditor()
+    d.create("/home/patrick/Desktop/KPIT", "file.xml", "Tree")  
+    d.update("/home/patrick/Desktop/KPIT/file.xml", "fruit", "grape")
+
+    # TODO:
+
+    '''
+    def createTextNode(self, data):
+        if not isinstance(data, StringTypes):
+            raise TypeError, "node contents must be a string"
     '''
