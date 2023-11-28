0705835d7aa75d0cd0f054ce2eddba945fd6b134
pat-jpnk <jpnksoftware@protonmail.com>
2023-11-28 07:50:51 +0100

    finalizing attempt, refactored code

--- FileEditor.py
+++ FileEditor.py
@@ -9,94 +9,65 @@ import uuid
 import xml.dom.minidom as md
 import xml.parsers
 
+
 '''
-Write a program to update/create a text file.
+limitations, features and interpretations of task:
 
-Program should be written using class in python.
+- text files can only be updated by appending new lines text
+- only xlsx excel format is targeted for simplicity, although readily extendable and likely functional with more
+  extensions with minimal changes
+- xml files can only be updated by adding new elements with text, one level deep, to the root element / XML of is validated before
+  updating new files
+- no experience with XML, except very limited Java Spring videos, might not be implemeted according to best practices (creating 
+  XML document without Document Type Definition, etc...)
+- excel files can only be updated by writing pandas DataFrames to new sheets / sheets must have unique name, causing error
+  otherwise, could be handled by adding "-{unique constant}" to name
+- written to be operating system agnostic (functionality across multiple operating systems) - not tested / also no unit
+  testing due to time issues / also no manual tests of excel due to Ubuntu operating system
+- correct file extensions, although not technically required in all cases are made mandatory
+- different file checking methods applied
+- automatic closing, also on exceptions, of files via using "with ... open(...)"
+- code was reflected in last hour to implement DRY principles, no manual testing was performed after due to time issues
 
-Program should have exception handling.
+needed additions:
+- unit testing
+- consistent use of "_" in variable names
+- better excel support
+- adding type hints
+- extending functionality
 
-Should develop the same program using minimum 2-3 different python modules.
-
---------------------------------------------------------------------------------
-
-Write a program to create/update an Excel file.
-
-Program should be written using class in python.
-
-Program should have exception handling.
-
-Should develop the same program using minimum 2-3 different python modules.
-
---------------------------------------------------------------------------------
-
-Write a program to create/update a xml file.
-
-Program should be written using class in python.
-
-Program should have exception handling.
-
-Should develop the same program using minimum 2-3 different python modules.
 '''
-
-
-# https://support.microsoft.com/en-au/office/file-formats-that-are-supported-in-excel-0943ff2c-6014-4e8d-aaea-b83d51d46247
-# https://pypi.org/project/filetype/
-# https://stackoverflow.com/questions/42778784/abstract-classes-with-varying-amounts-of-parameters
-
-# https://pythonbasics.org/write-excel/
-
-# with open closes file if an exception occurs
-
-
-# goals: cross operating system, extensibility, DRY
-
-# strict file extension requirements
-
-# notes
-
-# functionality and limitations (only xlsx, only adding xml elements with text one level deep, only appending to text file)
-
-# TODO: type hints
-# TODO: consistent underscores
-
 
 class FileEditor(ABC):
 
     file_extensions: list[str]
 
     @abstractmethod
-    def create(self, path, name):
+    def create(self):
         pass
 
     @abstractmethod
-    def update(self, filepath, content):
+    def update(self):
         pass
 
     @abstractmethod
-    def file_validation_function():
+    def file_validation_function(self):
         pass
-
-    '''
-    @abstractmethod
-    def verify_file_signature(self, file_validation_function, filepath, file_extensions):
-        if file_validation_function(file_path, file_extensions):
-            return True
-    '''
 
     def verify_file_extension(self, file_name, file_extensions):
         file_name_split = file_name.split('.')
-
         extension_str = ",".join(str(element) for element in file_extensions)
 
         if len(file_name_split) != 2 or file_name_split[1] not in file_extensions:
             raise ValueError("invalid filename, valid extensions: " + extension_str)
-
         else:
             return True
         
     def verify_file_signature(file_validation_function, file_path, file_extensions):
+        file_name = os.path.split(filepath)[1]
 
+        if FileEditor.verify_file_extension(file_name, file_extensions) and file_validation_function(file_path, file_extensions):
+            return True
 
 
 class TextFileEditor(FileEditor):
@@ -111,13 +82,6 @@ class TextFileEditor(FileEditor):
             raise FileNotFoundError("directory does not exist")
 
         # check file extension
-
-        '''
-        file_name_split = file_name.split('.')
-
-        if len(file_name_split) != 2 or file_name_split[1] not in TextFileEditor.file_extensions:
-            raise ValueError("invalid filename, .txt extension required")
-        '''
 
         if verify_file_extension(file_name, TextFileEditor.file_extensions):
             
@@ -141,17 +105,7 @@ class TextFileEditor(FileEditor):
 
         # check file extension and that file is not binary
 
-        '''
-        file_name_split = os.path.split(filepath)[1].split(".")
-
-        if len(file_name_split) != 2 or file_name_split[1] not in TextFileEditor.file_extensions:
-            raise ValueError("invalid filename, .txt extension required")
-
-        if is_binary(filepath):
-            raise ValueError("provided file is binary, text file required")
-        '''
-
-        if verify_file_signature(is_binary, filepath, TextFileEditor.file_extensions):
+        if verify_file_signature(self.file_validation_function, filepath, TextFileEditor.file_extensions):
 
             # add content
 
@@ -159,19 +113,6 @@ class TextFileEditor(FileEditor):
 
             with open(filepath, 'a') as f:
                 f.write(content)
-
-    '''
-    def file_validation_function2(filepath, file_extensions):
-        file_name_split = os.path.split(filepath)[1].split(".")
-
-        if len(file_name_split) != 2 or file_name_split[1] not in file_extensions:
-            raise ValueError("invalid filename, .txt extension required")
-
-        if is_binary(filepath):
-            raise ValueError("provided file is binary, text file required")
-        
-        return True
-    '''
 
     def file_validation_function(filepath, file_extensions):
         if is_binary(filepath):
@@ -182,7 +123,7 @@ class TextFileEditor(FileEditor):
 
 class ExcelFileEditor(FileEditor):
 
-    file_extensions = ["xlsx", "xlsm"]     # use list, more types ? TODO
+    file_extensions = ["xlsx"]
 
     def create(self, path, file_name):
         
@@ -192,13 +133,6 @@ class ExcelFileEditor(FileEditor):
             raise FileNotFoundError("directory does not exist")
         
         # validate file name
-        
-        '''
-        file_name_split = file_name.split('.')
-
-        if len(file_name_split) != 2 or file_name_split[1] not in ExcelFileEditor.file_extensions:      # TODO: limit to xlsx ? check reqs
-            raise ValueError("invalid filename, .xlsx extension required")
-        '''
 
         if verify_file_extension(file_name, TextFileEditor.file_extensions):
 
@@ -208,7 +142,7 @@ class ExcelFileEditor(FileEditor):
 
             df = pandas.DataFrame()
 
-            # TODO: check what if file exists
+            # TODO: check what if file already exists
 
             df.to_excel(file_path, index=False, header=False)
 
@@ -223,44 +157,31 @@ class ExcelFileEditor(FileEditor):
 
         # check that file exists and file signature is valid
 
-        if not os.path.isfile(filepath):
-            raise FileNotFoundError("file does not exists")
+        if verify_file_signature(self.file_validation_function, filepath, ExcelFileEditor.file_extensions):
 
+            # update file
+
+            with pandas.ExcelWriter(filepath) as w:
+                try:
+                    w.book = openpyxl.load_workbook(filepath)
+                    data.to_excel(w, sheet_name=sheet_id)
+                except (IllegalCharacterError, SheetTitleException, InvalidFileException, ReadOnlyWorkbookException) as error:
+                    print(error)
+
+
+    def file_validation_function(filepath, file_extensions):
         file_type = filetype.guess_extension(filepath)
 
         if file_type:
-            if file_type not in ExcelFileEditor.file_extensions:
+            if file_type not in file_extensions:
                 raise ValueError("file type not accepted")
+            else:
+                return True
         else:
             raise ValueError("file type indeterminate")
 
-        # check that file extension is valid
-
-        file_name_split = os.path.split(filepath)[1].split(".")
-
-        if len(file_name_split) != 2 or file_name_split[1] not in ExcelFileEditor.file_extensions:
-            raise ValueError("invalid filename, .xlsx extension required")
 
 
-        # https://openpyxl.readthedocs.io/en/stable/_modules/openpyxl/utils/exceptions.html#InvalidFileException
-
-        with pandas.ExcelWriter(filepath) as w:
-            try:
-                w.book = openpyxl.load_workbook(filepath)
-                data.to_excel(w, sheet_name=sheet_id)
-            except (IllegalCharacterError, SheetTitleException, InvalidFileException, ReadOnlyWorkbookException) as error:
-                print(error)
-
-    def verify_file_extension(self, filepath):
-        pass
-
-    def verify_file_signature(self, filepath):
-        pass
-
-
-# creating XML document without Document Type Definition
-# https://bspaans.github.io/python-mingus/_modules/xml/dom/minidom.html
-# https://github.com/python/cpython/blob/main/Lib/xml/dom/expatbuilder.pys
 class XMLFileEditor(FileEditor):
 
     file_extensions = ["xml"]
@@ -273,13 +194,6 @@ class XMLFileEditor(FileEditor):
             raise FileNotFoundError("directory does not exist")
         
         # validate file extension
-
-        '''
-        file_name_split = file_name.split('.')
-
-        if len(file_name_split) != 2 or file_name_split[1] not in XMLFileEditor.file_extensions:   
-            raise ValueError("invalid filename, .xml extension required")
-        '''
 
         if verify_file_extension(file_name, TextFileEditor.file_extensions):
 
@@ -303,68 +217,55 @@ class XMLFileEditor(FileEditor):
 
     def update(self, filepath, element_name, element_text):
 
-        # check that file exists and is not binary
+        # check that file exists
 
         if not os.path.isfile(filepath):
             raise FileNotFoundError("file does not exists")
         
+        # check that file is not binary and that file extension is valid
+
+        if verify_file_signature(self.file_validation_function, filepath, ExcelFileEditor.file_extensions):
+
+            # read contents and verify that it is valid XML
+
+            with open(filepath, 'r') as f:
+                file_content = f.read()
+            
+            try:
+                content_dom = md.parseString(file_content)
+            except xml.parsers.expat.ExpatError as error:
+                print(error)
+
+            # update file contents
+
+            root_element = content_dom.documentElement
+            new_element = content_dom.createElement(element_name)
+            new_element_text = content_dom.createTextNode(element_text)
+            root_element.appendChild(new_element)
+            new_element.appendChild(new_element_text)
+
+            xml_content = content_dom.toprettyxml(indent='\t')
+
+            with open(filepath, 'w') as f:
+                f.write(xml_content)
+
+
+    def file_validation_function(filepath, file_extensions):
         if is_binary(filepath):
             raise ValueError("provided file is binary, text file required")
-
-        # check that file extension is valid
-
-        file_name_split = os.path.split(filepath)[1].split(".")
-
-        if len(file_name_split) != 2 or file_name_split[1] not in XMLFileEditor.file_extensions:
-            raise ValueError("invalid filename, .xml extension required")
-
-        # read contents and verify that it is valid XML
-
-        with open(filepath, 'r') as f:
-            file_content = f.read()
-        
-        try:
-            content_dom = md.parseString(file_content)
-        except xml.parsers.expat.ExpatError as error:
-            print(error)
-
-        # update file contents
-
-        root_element = content_dom.documentElement
-        new_element = content_dom.createElement(element_name)
-        new_element_text = content_dom.createTextNode(element_text)
-        root_element.appendChild(new_element)
-        new_element.appendChild(new_element_text)
-
-        xml_content = content_dom.toprettyxml(indent='\t')
-
-        with open(filepath, 'w') as f:
-            f.write(xml_content)
+        else:
+            return True
 
 
-    def verify_file_extension(self, filepath):
-        pass
+'''
+useful links:
 
-    def verify_file_signature(self, filepath):
-        pass
+https://openpyxl.readthedocs.io/en/stable/_modules/openpyxl/utils/exceptions.html#InvalidFileException
+https://support.microsoft.com/en-au/office/file-formats-that-are-supported-in-excel-0943ff2c-6014-4e8d-aaea-b83d51d46247
+https://pypi.org/project/filetype/
+https://stackoverflow.com/questions/42778784/abstract-classes-with-varying-amounts-of-parameters
+https://pythonbasics.org/write-excel/
+https://bspaans.github.io/python-mingus/_modules/xml/dom/minidom.html
+https://github.com/python/cpython/blob/main/Lib/xml/dom/expatbuilder.pys
 
-
-if __name__ == "__main__":
-    '''
-    d = ExcelFileEditor()
-    d.create("/home/patrick/Desktop/KPIT", "file.xlsx")
-
-    d.update("/home/patrick/Desktop/KPIT/file.xlsx", "Hi")
-    '''
-
-    d = XMLFileEditor()
-    d.create("/home/patrick/Desktop/KPIT", "file.xml", "Tree")  
-    d.update("/home/patrick/Desktop/KPIT/file.xml", "fruit", "grape")
-
-    # TODO:
-
-    '''
-    def createTextNode(self, data):
-        if not isinstance(data, StringTypes):
-            raise TypeError, "node contents must be a string"
-    '''
+'''
